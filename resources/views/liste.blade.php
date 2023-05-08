f76b2146cf66cfc57c35c3864030bed5b71ac42a
Orlando0309 <andrianayorllando@gmail.com>
2023-05-08 16:49:55 +0300

    Update liste.blade.php

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -147,8 +147,8 @@
                   <p>{{ $l->nomcategorie }}</p>
                 </div>
                 <div class="meta">
-                  {{$l->datecreation}}
-                  {{-- <time class="published" datetime="{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $l->datecreation)->format('Y-m-d') }}">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $l->datecreation)->format('F d, Y') }}</time> --}}
+                
+                  <time class="published" datetime="{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s.u', $l->datecreation)->format('Y-m-d') }}">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s.u', $l->datecreation)->format('F d, Y') }}</time>
                 </div>
               </header>
               <a href="/article/{{$l->id.'-'.$l->slug}}" class="image featured"><img src="{{ $l->image }}" alt="" /></a>
@@ -189,7 +189,7 @@
                   <article class="mini-post">
                     <header>
                       <h3><a href="/article/{{$l->id.'-'.$l->slug}}">{{ $l->titre }}</a></h3>
-                      {{-- <time class="published" datetime="{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $l->datecreation)->format('Y-m-d') }}">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $l->datecreation)->format('F d, Y') }}</time> --}}
+                      <time class="published" datetime="{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s.u', $l->datecreation)->format('Y-m-d') }}">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s.u', $l->datecreation)->format('F d, Y') }}</time>
                       <a class="author" href="/article/{{$l->id.'-'.$l->slug}}" class="image featured"><img src="{{ $l->image }}" alt="" /></a>
                     </header>
                     <a class="image" href="/article/{{$l->id.'-'.$l->slug}}" class="image featured"><img src="{{ $l->image }}" alt="" /></a>
