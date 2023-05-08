d4293e14eb61c0fdddb4f2defa68add2257ed499
Orlando0309 <andrianayorllando@gmail.com>
2023-05-08 12:47:18 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ use App\Http\Middleware\CheckUserRole;
 
 // Route::get('/article/', [ArticleController::class,"create"]);
 
-Route::get('/', [ArticleController::class,"list"])->name('start');
+Route::get('/', [ArticleController::class,"list"]);
+Route::get('/log', [ArticleController::class,"start"])->name('start');
 Route::post('/admin',[ArticleController::class,"logadmin"])->name('logadmin');
 Route::prefix('article')->group(function () {
     Route::get('/', [ArticleController::class,"create"]);
